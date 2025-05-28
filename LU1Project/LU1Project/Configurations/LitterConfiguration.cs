7ea9e5b35dd2b2d3d0dc86a54a550689dab45dc2
mrayan007 <rayanwmv@gmail.com>
2025-05-28 18:23:59 +0200

    commit

---
+++ LU1Project/LU1Project/Configurations/LitterConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LU1Project.Models;
+
+namespace LU1Project.Configurations
+{
+    public class LitterConfiguration : IEntityTypeConfiguration<Litter>
+    {
+        public void Configure(EntityTypeBuilder<Litter> builder)
+        {
+            builder.ToTable("Litter");
+
+            builder.HasData(
+                new Litter
+                {
+                    Id = 1,
+                    Color = "Yellow",
+                    Description = "Een mooie kleur",
+                    Location = "Avans",
+                    ReportDate = new DateTime(2025, 5, 15)
+                }
+            );
+        }
+    }
+}
---
+++ LU1Project/LU1Project/Migrations/20250521115659_SeedLitterData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LU1Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedLitterData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
